bf8f03bb76df69c3e339a3bb6769c13b7567f3f4
Sean Purser-Haskell <sean.purserhaskell@gmail.com>
2015-09-28 14:43:19 +0800

    - No libraries in test

--- motor_encoder_test/motor_encoder_test.ino
+++ motor_encoder_test/motor_encoder_test.ino
@@ -1,26 +1,50 @@
 
-#include <DCMotor.h>
-
 const int sEncoderPins[] = {0, 1};
 const int sEncoderCount = sizeof(sEncoderPins) / sizeof(*sEncoderPins);
 
-int M0EN = 12;
-int M01  = 19;
-int M02 =  20;
-
-int M1EN =  13;
-int M11 =  21;
-int M12 =  22;
-
-DCMotor motor0(M0EN, M01, M02);
-DCMotor motor1(M1EN, M11, M12);
+const int sMotorCount = 2;
+const int sMotorEnable[sMotorCount] = {12, 13};
+const int sMotorPositive[sMotorCount]  = {19, 21};
+const int sMotorNegative[sMotorCount] =  {20, 22};
 
 void setup() {
   for(int i=0;i<sEncoderCount;++i) {
     pinMode(sEncoderPins[i], INPUT);
     digitalWrite(sEncoderPins[i], HIGH);
   }
+  for(int i=0;i<2;++i) {
+    pinMode(sMotorEnable[i], OUTPUT);
+    pinMode(sMotorPositive[i], OUTPUT);
+    pinMode(sMotorNegative[i], OUTPUT);
+  }
+  
   Serial.begin(9600);
+}
+
+void stop_all_motors()
+{
+  for(int i=0;i<2;++i) {
+    digitalWrite(sMotorEnable[i], LOW);
+    digitalWrite(sMotorPositive[i], LOW);
+    digitalWrite(sMotorNegative[i], LOW);
+  }
+  delayMicroseconds(100);
+}
+
+void run_motor(int m, bool dir) 
+{
+  digitalWrite(sMotorPositive[m], LOW);
+  digitalWrite(sMotorNegative[m], LOW);
+  delayMicroseconds(100);
+  digitalWrite(sMotorEnable[m], HIGH);
+  digitalWrite(sMotorPositive[m], dir ? HIGH : LOW);
+  digitalWrite(sMotorNegative[m], dir ? LOW : HIGH);
+  delayMicroseconds(100);
+}
+
+void stop_motor(int m) {
+  digitalWrite(sMotorEnable[m], LOW);
+  delayMicroseconds(100);
 }
 
 void count_encoders_for(int ms)
@@ -57,16 +81,15 @@ void loop() {
       Serial.print(m1);
       Serial.println();
       if(m0)
-        motor0.setSpeed(80);
+        run_motor(0, true);
       else
-        motor0.brake();
+        stop_motor(0);
       if(m1)
-        motor1.setSpeed(80);
+        run_motor(1, true);
       else 
-        motor1.brake();
+        stop_motor(1);
       count_encoders_for(2000);
-      motor0.brake();
-      motor1.brake();
+      stop_all_motors();
       delay(1000);
     }
   }
